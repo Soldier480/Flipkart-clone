e360ac929ed2fa375488907a3674f3259a17cb14
Soldier480 <123284598+Soldier480@users.noreply.github.com>
2024-03-16 15:36:40 +0530

    Update productActions.js

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as actionTypes from '../constants/productConstant.js'
-const URL="flipkart-clone-api-iota.vercel.app";
+const URL="https://flipkart-clone-api-iota.vercel.app";
 
 
   export const getProducts =()=> async (dispatch) => {//syntax to pass to middleware
