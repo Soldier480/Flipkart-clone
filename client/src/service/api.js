4fddd10311a96ec7d106234c838d0891a05339a2
Soldier480 <123284598+Soldier480@users.noreply.github.com>
2024-03-16 14:55:56 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
   
-const url = 'http://localhost:8000'
+const url = 'flipkart-clone-api-iota.vercel.app'
 
 export const authenticateSignup = async (data) => {
     try {
